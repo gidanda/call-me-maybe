023fbc50ae3c101d0590bc78811c419205fc4b75
Hiroki Goda <hirokigoda@HirokinoMacBook-Air.local>
2026-09-11 14:35:46 +0900

    finish experiments

--- experiments/basics.py
+++ experiments/basics.py
@@ -2,10 +2,17 @@ from llm_sdk import Small_LLM_Model
 
 def main():
     model = Small_LLM_Model()
-    ids = model.encode("I am").tolist()[0]
+    ids = model.encode("I like").tolist()[0]
+
+    allowed_ids = {10, 20}
+
+    logits = model.get_logits_from_input_ids(ids)
+    for token_id in range(len(logits)):
+            if token_id not in allowed_ids:
+                logits[token_id] = float("-inf")
     
-    for i in range(10):
-        logits = model.get_logits_from_input_ids(ids)
+    for i in range(1):
+        # logits = model.get_logits_from_input_ids(ids)
         tmp_best = logits[0]
         for logit in logits:
             if logit > tmp_best:
@@ -15,6 +22,8 @@ def main():
         ids.append(best_id)
 
     output = model.decode(ids)
+    print(repr(model.decode([10])))
+    print(repr(model.decode([20])))
 
     print(repr(output))
 
